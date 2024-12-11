b105bfa859ea50e48fd4846301691f1198866a19
Dwayne Dyer <iStagnate@gmail.com>
2024-12-11 09:06:02 -0500

    Added loot tables to drops as well as creative tabs

--- src/main/java/net/iStagnate/mobular_mechanisms/MobMechs.java
+++ src/main/java/net/iStagnate/mobular_mechanisms/MobMechs.java
@@ -1,6 +1,7 @@
 package net.iStagnate.mobular_mechanisms;
 
 import net.iStagnate.mobular_mechanisms.block.ModBlocks;
+import net.iStagnate.mobular_mechanisms.item.ModCreativeModeTabs;
 import net.iStagnate.mobular_mechanisms.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -36,6 +37,8 @@ public class MobMechs {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
